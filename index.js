f65a3779f59d35db5bdee9de6a44b104c7bc00cb
anthonysalas11 <anthony.salas.ts@gmail.com>
2025-10-01 15:34:34 -0400

    sixth function

--- index.js
+++ index.js
@@ -40,3 +40,14 @@ const multiply = (num1, num2) => {
 }
 
 // multiply(3, 7.5)
+
+
+
+const add = (num1, num2) => {
+    if (num1 !== num2) {
+        return num1 + num2
+    } else {
+        return num1 + num2 * 3
+    }
+}
+console.log(add(10, -5))
